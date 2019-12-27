d8560677f0992907e934f8f4bd26db69a18ec387
SFoskitt <stephanie.foskitt@gmail.com>
2019-12-26 21:57:02 -0600

    my updates to this script

--- beesight.py
+++ beesight.py
@@ -1,27 +1,26 @@
+# forked
 import ConfigParser
 import datetime
 import urllib
 import urllib2
 import requests
+import ssl
 import sys
 import simplejson
 import time
-
-# complain on config file issues
-# complain on bad login
-# don't hardcode timezone to japan
 
 CONFIG_FILE_NAME = 'config.ini'
 INSIGHT_SECTION = 'insight'
 BEEMINDER_SECTION = 'beeminder'
 
-LOGIN_URL = "https://insighttimer.com/user_session"
-INSIGHT_CSV_URL = "https://insighttimer.com/users/export"
+INSIGHT_LOGIN_URL = "https://profile.insighttimer.com/profile_signin/request"
+INSIGHT_CSV_URL = "https://profile.insighttimer.com/sessions/export"
 
-BASE_URL= "https://www.beeminder.com/api/v1/"
-GET_DATAPOINTS_URL = BASE_URL + "users/%s/goals/%s/datapoints.json?auth_token=%s"
-POST_MANY_DATAPOINTS_URL = BASE_URL + "users/%s/goals/%s/datapoints/create_all.json?auth_token=%s"
-POST_DATAPOINTS_URL = GET_DATAPOINTS_URL + "&timestamp=%s&value=%s"
+BEE_BASE_URL = "https://www.beeminder.com/api/v1/"
+BEE_GET_DATAPOINTS_URL = BEE_BASE_URL  + "users/%s/goals/%s/datapoints.json?auth_token=%s"
+# POST_MANY_DATAPOINTS_URL = BEE_BASE_URL  + "users/%s/goals/%s/datapoints/create_all.json?auth_token=%s"
+BEE_POST_DATAPOINTS_URL = BEE_GET_DATAPOINTS_URL + "&timestamp=%s&value=%s&comment=%s"
+print "BEE_POST_DATAPOINTS_URL  %s" %BEE_POST_DATAPOINTS_URL
 
 def get_insight_data():
     config = ConfigParser.RawConfigParser()
@@ -29,42 +28,45 @@ def get_insight_data():
 
     username = config.get(INSIGHT_SECTION, "username")
     password = config.get(INSIGHT_SECTION, "password")
-
+    
     values = {'user_session[email]' : username,
               'user_session[password]' : password }
     login_data = urllib.urlencode(values)
 
     # Start a session so we can have persistent cookies
     session = requests.session()
-    r = session.post(LOGIN_URL, data=login_data)
+    r = session.post(INSIGHT_LOGIN_URL, data=login_data)
     r = session.get(INSIGHT_CSV_URL)
     return r.text.split('\n')
 
 def post_beeminder_entry(entry):
-        config = ConfigParser.RawConfigParser()
-        config.read(CONFIG_FILE_NAME)
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_NAME)
 
-        username = config.get(BEEMINDER_SECTION, "username")
-        auth_token = config.get(BEEMINDER_SECTION, "auth_token")
-        goal_name = config.get(BEEMINDER_SECTION, "goal_name")
+    username = config.get(BEEMINDER_SECTION, "username")
+    auth_token = config.get(BEEMINDER_SECTION, "auth_token")
+    goal_name = config.get(BEEMINDER_SECTION, "goal_name")
 
-        session = requests.session()
-        full_url = POST_DATAPOINTS_URL % (username, goal_name, auth_token, entry["timestamp"], entry["value"])
-        r = session.post(full_url)
+    session = requests.session()
+    full_url = BEE_POST_DATAPOINTS_URL % (username, goal_name, auth_token, entry["timestamp"], entry["value"], entry["comment"])
+    r = session.post(full_url)
 
-        print "Posted entry: %s" % r.text
+    print "Posted entry: %s" % r.text
 
 def get_beeminder():
-        config = ConfigParser.RawConfigParser()
-        config.read(CONFIG_FILE_NAME)
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_NAME)
 
-        username = config.get(BEEMINDER_SECTION, "username")
-        auth_token = config.get(BEEMINDER_SECTION, "auth_token")
-        goal_name = config.get(BEEMINDER_SECTION, "goal_name")
+    username = config.get(BEEMINDER_SECTION, "username")
+    auth_token = config.get(BEEMINDER_SECTION, "auth_token")
+    goal_name = config.get(BEEMINDER_SECTION, "goal_name")
+    bee_data_url = BEE_GET_DATAPOINTS_URL % (username, goal_name, auth_token)
+    # print "Bee data url: %s" % bee_data_url
 
-        response = urllib2.urlopen(GET_DATAPOINTS_URL % (username, goal_name, auth_token))
-        the_page = response.read()
-        return the_page
+    context = ssl._create_unverified_context()
+    response = urllib2.urlopen(bee_data_url, context=context)
+    the_page = response.read()
+    return the_page
 
 def beeminder_to_one_per_day(beeminder_output):
     bm = simplejson.loads(beeminder_output)
@@ -76,9 +78,9 @@ def beeminder_to_one_per_day(beeminder_output):
         ts = entry['timestamp']
         dt = datetime.datetime.fromtimestamp(ts)
 
-        # need to move back one dayfrom the beeminder time, because it
+        # need to move back one day from the beeminder time, because it
         # pushes the day forward to 01:00 on day + 1, at least in JST
-        d = dt.date() - datetime.timedelta(days=1)
+        d = dt.date()
 
         if not d in s:
             s[d] = 1
@@ -116,12 +118,27 @@ if __name__ == "__main__":
     print "%s datapoints in beeminder" % len(beeminder_dates)
 
     # get dates which beeminder doesn't know about yet
+    bee_sorted = sorted(set(beeminder_dates))
+    for d in bee_sorted:
+      print "bee sorted: %s" %d
+
+    insight_sorted = sorted(set(insight_dates))
+    for d in insight_sorted:
+      print "insight sorted: %s" %d
+    inter = list(set(insight_dates) - set(beeminder_dates))
+    for d in inter:
+      print "inter list date: %s" % d
+
     new_dates = sorted(list(set(insight_dates) - set(beeminder_dates)))
+    print "new dates len: %s" % len(new_dates)
+    for d in new_dates:
+      print "date in new_dates: %s" % d
 
     # create beeminder-friendly datapoints
-    new_datapoints = [{'timestamp': date_to_jp_timestamp(d), 'value':1.0, "comment":"test"} for d in new_dates]
-
+    new_datapoints = [{"timestamp": d, "value":1.0, "comment":"Zapier to Heroku to InsightsTimer to Beeminder"} for d in new_dates]
+    for d in new_datapoints:
+        print "datapoint in new_datapoints: %s" % d
     print "%s datapoints to post" % len(new_datapoints)
 
-    for dp in new_datapoints:
-        post_beeminder_entry(dp)
+    # for dp in new_datapoints:
+    #     post_beeminder_entry(dp)
