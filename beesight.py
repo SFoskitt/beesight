06ab1ca883eb12589019c540920d869642db1388
SFoskitt <stephanie.foskitt@gmail.com>
2019-12-30 19:03:11 -0600

    run this locally with local config.ini

--- beesight.py
+++ beesight.py
@@ -1,5 +1,5 @@
-# forked
-# import ConfigParser
+# mine
+import ConfigParser
 import datetime
 import urllib
 import urllib2
@@ -8,71 +8,72 @@ import ssl
 import sys
 import simplejson
 import time
-import S3Connection
 
+# complain on config file issues
+# complain on bad login
+# don't hardcode timezone to japan
 
-# insight_username = S3Connection(os.environ['insight_username'], os.environ['S3_SECRET'])
+CONFIG_FILE_NAME = 'config.ini'
+INSIGHT_SECTION = 'insight'
+BEEMINDER_SECTION = 'beeminder'
 
+# LOGIN_URL = "https://insighttimer.com/user_session"
+# INSIGHT_CSV_URL = "https://insighttimer.com/users/export"
 
-# CONFIG_FILE_NAME = 'config.ini'
 INSIGHT_LOGIN_URL = "https://profile.insighttimer.com/profile_signin/request"
 INSIGHT_CSV_URL = "https://profile.insighttimer.com/sessions/export"
 
 BEE_BASE_URL = "https://www.beeminder.com/api/v1/"
 BEE_GET_DATAPOINTS_URL = BEE_BASE_URL  + "users/%s/goals/%s/datapoints.json?auth_token=%s"
 # POST_MANY_DATAPOINTS_URL = BEE_BASE_URL  + "users/%s/goals/%s/datapoints/create_all.json?auth_token=%s"
-BEE_POST_DATAPOINTS_URL = BEE_GET_DATAPOINTS_URL + "&timestamp=%s&value=%s&%s"
-# print "BEE_POST_DATAPOINTS_URL  %s" %BEE_POST_DATAPOINTS_URL
+# BEE_POST_DATAPOINTS_URL = BEE_GET_DATAPOINTS_URL + "&timestamp=%s&value=%s"
 
 def get_insight_data():
-    # config = ConfigParser.RawConfigParser()
-    # config.read(CONFIG_FILE_NAME)
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_NAME)
 
-    # username = config.get("app", "insight_username")
-    # password = config.get("app", "insight_password")
-
-    username = S3Connection(os.environ['INSIGHT_USERNAME'])
-    password = S3Connection(os.environ['INSIGHT_PASSWORD'])
+    username = config.get(INSIGHT_SECTION, "username")
+    password = config.get(INSIGHT_SECTION, "password")
+    
+    # GET THESE FROM HEROKU ENV VAR
+    # username = "email@liquidsilvers.com"
+    # password = "hottub"
 
     values = {'user_session[email]' : username,
               'user_session[password]' : password }
     login_data = urllib.urlencode(values)
+    # print "insight login: %s" % login_data
 
     # Start a session so we can have persistent cookies
     session = requests.session()
     r = session.post(INSIGHT_LOGIN_URL, data=login_data)
     r = session.get(INSIGHT_CSV_URL)
-    return r.text.split('\n')
+    arr = r.text.split('\n')
+    # for a in arr:
+    #   print "insight line: %s" %a
+    return arr
 
 def post_beeminder_entry(entry):
-    # config = ConfigParser.RawConfigParser()
-    # config.read(CONFIG_FILE_NAME)
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_NAME)
 
-    # username = config.get("app", "beeminder_username")
-    # goal_name = config.get("app", "beeminder_goal_name")
-    # auth_token = config.get("app", "beeminder_auth_token")
-
-    username = S3Connection(os.environ['BEEMINDER_USERNAME'])
-    goal_name = S3Connection(os.environ['BEEMINDER_GOAL_NAME'])
-    auth_token = S3Connection(os.environ['BEEMINDER_AUTH_TOKEN'])
+    username = config.get(BEEMINDER_SECTION, "username")
+    auth_token = config.get(BEEMINDER_SECTION, "auth_token")
+    goal_name = config.get(BEEMINDER_SECTION, "goal_name")
 
     session = requests.session()
-    comment_encoded = urllib.urlencode({"comment": entry["comment"]})
-    full_url = BEE_POST_DATAPOINTS_URL % (username, goal_name, auth_token, entry["timestamp"], entry["value"], comment_encoded)
-    # print "full post url:  %s" %full_url
-
-    # print "comment_encoded: %s" % comment_encoded
-
+    full_url = BEE_POST_DATAPOINTS_URL % (username, goal_name, auth_token, entry["timestamp"], entry["value"])
     r = session.post(full_url)
+
     print "Posted entry: %s" % r.text
 
 def get_beeminder():
-    # config = ConfigParser.RawConfigParser()
-    # config.read(CONFIG_FILE_NAME)
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE_NAME)
 
-    username = S3Connection(os.environ['BEEMINDER_USERNAME'])
-    goal_name = S3Connection(os.environ['BEEMINDER_GOAL_NAME'])
-    auth_token = S3Connection(os.environ['BEEMINDER_AUTH_TOKEN'])
+    username = config.get(BEEMINDER_SECTION, "username")
+    auth_token = config.get(BEEMINDER_SECTION, "auth_token")
+    goal_name = config.get(BEEMINDER_SECTION, "goal_name")
     bee_data_url = BEE_GET_DATAPOINTS_URL % (username, goal_name, auth_token)
     # print "Bee data url: %s" % bee_data_url
 
@@ -132,27 +133,26 @@ if __name__ == "__main__":
 
     # get dates which beeminder doesn't know about yet
     bee_sorted = sorted(set(beeminder_dates))
-    # for d in bee_sorted:
-    #   print "bee sorted: %s" %d
+    for d in bee_sorted:
+      print "bee sorted: %s" %d
 
     insight_sorted = sorted(set(insight_dates))
-    # for d in insight_sorted:
-    #   print "insight sorted: %s" %d
-
+    for d in insight_sorted:
+      print "insight sorted: %s" %d
     inter = list(set(insight_dates) - set(beeminder_dates))
-    # for d in inter:
-    #   print "inter list date: %s" % d
+    for d in inter:
+      print "inter list date: %s" % d
 
     new_dates = sorted(list(set(insight_dates) - set(beeminder_dates)))
     print "new dates len: %s" % len(new_dates)
-    # for d in new_dates:
-    #   print "date in new_dates: %s" % d
+    for d in new_dates:
+      print "date in new_dates: %s" % d
 
     # create beeminder-friendly datapoints
-    new_datapoints = [{"timestamp": d, "value":1, "comment":"Zapier to Heroku to InsightsTimer to Beeminder"} for d in new_dates]
-    # for d in new_datapoints:
-    #     print "datapoint in new_datapoints: %s" % d
+    new_datapoints = [{"timestamp": d, "value":1.0, "comment":"Zapier to Heroku to InsightsTimer to Beeminder"} for d in new_dates]
+    for d in new_datapoints:
+        print "datapoint in new_datapoints: %s" % d
     print "%s datapoints to post" % len(new_datapoints)
 
     # for dp in new_datapoints:
-    post_beeminder_entry({"timestamp": 1577419913, "value":1, "comment":"Zapier to Heroku to InsightsTimer to Beeminder"})
+    #     post_beeminder_entry(dp)
